ef69127ff16f0701cb700514e0d291f211e19fac
skiwithpete <skiwithpete@users.noreply.github.com>
2020-05-28 19:09:11 -0400

    Update timelapse.py

--- simple/timelapse.py
+++ simple/timelapse.py
@@ -30,7 +30,7 @@ while (FrameCount < FrameStop):
 
 # create film
 print('converting to film now')
-system('avconv -r 24 -i image%04d.jpg -vcodec libx264 -crf 20 -g 15 `date +%Y%m%d%H%M`timelapse.mp4')
+system('ffmpeg -r 24 -i image%04d.jpg -vcodec libx264 -crf 20 -g 15 `date +%Y%m%d%H%M`timelapse.mp4')
 
 # create film
 print('moving completed mp4 file')
